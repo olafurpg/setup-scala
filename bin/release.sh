5fbc7abe97954a5348df0397160d921ad65d2bd6
Ólafur Páll Geirsson <olafurpg@gmail.com>
2019-09-22 12:09:14 +0100

    Automate releases

---
+++ bin/release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eux
+git add .
+git diff
+version=$1
+git checkout -b releases/$version
+sed '/node_modules/d' .gitignore
+yarn run build
+git add .
+git commit -m "Generate artifacts"
+git push origin releases/$version
+git co master
+rm -rf node_modules
+yarn install
