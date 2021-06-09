5d3e1456f654ba33c69c674aa80add260aaab127
Adrien Piquerez <adrien.piquerez@gmail.com>
2021-06-09 14:02:47 +0200

    Install Coursier and sbtn

--- src/install.ts
+++ src/install.ts
@@ -1,14 +1,23 @@
 import * as core from "@actions/core";
 import * as shell from "shelljs";
 import * as path from "path";
+import * as fs from "fs";
 
 const homedir = require("os").homedir();
 const bin = path.join(homedir, "bin");
+const runnerOs = (shell.env["RUNNER_OS"] || "undefined").toLowerCase();
+const isWindows = runnerOs === "windows";
+
 
 export async function install(javaVersion: string, jabbaVersion: string) {
   setEnvironmentVariableCI();
+  shell.mkdir(bin);
+  core.addPath(bin);
   installJava(javaVersion, jabbaVersion);
   installSbt();
+  const cs = installCoursier();
+  if (!cs) return;
+  installSbtn(cs);
 }
 
 function setEnvironmentVariableCI() {
@@ -16,8 +25,7 @@ function setEnvironmentVariableCI() {
 }
 
 function jabbaUrlSuffix(): string {
-  const runnerOs = shell.env["RUNNER_OS"] || "undefined";
-  switch (runnerOs.toLowerCase()) {
+  switch (runnerOs) {
     case "linux":
       const arch = shell.exec("uname -m", { silent: true }).stdout.trim();
       switch (arch) {
@@ -39,20 +47,16 @@ function jabbaUrlSuffix(): string {
   }
 }
 
-function isWindows(): boolean {
-  return shell.env["RUNNER_OS"] === "Windows";
-}
-
 function jabbaName(): string {
-  if (isWindows()) return "jabba.exe";
+  if (isWindows) return "jabba.exe";
   else return "jabba";
 }
 
 function installJava(javaVersion: string, jabbaVersion: string) {
   core.startGroup("Install Java");
-  core.addPath(bin);
+  
   const jabbaUrl = `https://github.com/shyiko/jabba/releases/download/${jabbaVersion}/jabba-${jabbaVersion}-${jabbaUrlSuffix()}`;
-  shell.mkdir(bin);
+  
   const jabba = path.join(bin, jabbaName());
   shell.set("-ev");
   shell.exec(`curl -sL -o ${jabba} ${jabbaUrl}`, { silent: true });
@@ -112,7 +116,6 @@ function installJavaByExactVersion(javaVersion: string): JabbaInstall {
 
 function installSbt() {
   core.startGroup("Install sbt");
-  core.addPath(bin);
   curl(
     "https://raw.githubusercontent.com/paulp/sbt-extras/master/sbt",
     path.join(bin, "sbt")
@@ -129,4 +132,56 @@ function curl(url: string, outputFile: string) {
   shell.chmod(755, outputFile);
   shell.cat(outputFile);
   console.log(`Downloaded '${path.basename(outputFile)}' to ${outputFile}`);
+}
+
+function installCoursier(): string {
+  core.startGroup("Install coursier");
+  const cs = path.join(bin, coursierName());
+  const csUrl = `https://git.io/coursier-cli-${coursierUrlSuffix()}`
+  shell.exec(`curl -sL -o ${cs} ${csUrl}`, { silent: true });
+  shell.chmod(755, cs);
+  core.endGroup();
+  return cs;
+}
+
+function coursierUrlSuffix(): string {
+  switch (runnerOs) {
+    case "linux":
+      return "linux"
+    case "macos":
+      return "darwin";
+    case "windows":
+      return "windows-exe";
+    default:
+      throw new Error(
+        `unknown runner OS: ${runnerOs}, expected one of linux, macos or windows.`
+      );
+  }
+}
+
+function coursierName(): string {
+  if (isWindows) return "cs.exe";
+  else return "cs";
+}
+
+function installSbtn(cs: string) {
+  core.startGroup("Install sbtn");
+  const result = shell.exec(`${cs} install sbtn --dir ${bin}`);
+  if (result.code > 0) {
+    core.setFailed(
+      `Failed to install sbtn, cs stderr: ${result.stderr}`
+    );
+    return;
+  }
+  if (isWindows) {
+    // create an sbtn bash script on Windows
+    // so that we can call `sbtn` instead of `sbtn.bat`
+    const sbtn = `${bin}/sbtn`
+    const script = `
+    #!/bin/bash 
+    exec sbtn.bat "$@"
+    `
+    fs.writeFileSync(sbtn, script)
+  }
+  core.endGroup();
 }
